4f7bea1b796e19f98c2f17fd31862e313fc4801e
Franklin okeh <frankok101@gmail.com>
2018-04-23 13:05:37 +0100

    Update ReadCSVServlet.java

--- ReadCSVServlet.java
+++ ReadCSVServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -14,85 +9,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-/**
- *
- * @author Shedrach
- */
 public class ReadCSVServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
        
-       // String file= request.getParameter("file");
+      
         Part file=request.getPart("numbersList");
         String line = "";
         String cvsSplitBy = ",";
 
-//        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-//
-//            while ((line = br.readLine()) != null) {
-//
-//                // use comma as separator
-//                String[] phone = line.split(cvsSplitBy);
-//
-//                System.out.println("Country [code= " + phone[0] + " , name=" + phone[5] + "]");
-//request.setAttribute("phone", phone);
-//request.getRequestDispatcher("http://41.73.252.230:8080/SmartWallet/Proxy/ChangePinSergovia").forward(request, response);
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+   
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
